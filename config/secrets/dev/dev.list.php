70818f7f72a73d36385d07adeade8127efd65efe
Koched92 <hichem.koched@gmail.com>
2022-01-15 22:04:00 +0100

    crypt config

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'DATABASE_DRIVER_DEV' => null,
+    'DATABASE_HOST_DEV' => null,
+    'DATABASE_NAME_DEV' => null,
+    'DATABASE_PASSWORD_DEV' => null,
+    'DATABASE_PORT_DEV' => null,
+    'DATABASE_USER_DEV' => null,
+];
