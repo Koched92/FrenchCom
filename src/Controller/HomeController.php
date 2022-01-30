8544a5a340ac099a07dd961845e69ea3eab99e5f
Koched92 <hichem.koched@gmail.com>
2022-01-30 14:27:48 +0100

    fix security and last push from windows bye

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
+    
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
