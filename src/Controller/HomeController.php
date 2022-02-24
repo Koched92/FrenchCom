7fb2e81c95ebc9dec278fc04406533078fa04e5a
Koched92 <hichem.koched@gmail.com>
2022-02-24 22:52:16 +0100

    maintenance & templating & fosJsRouting

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Decision;
+use App\Entity\NatureDossier;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $user = $this->getUser();
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
