8ee7e92e190c06ab10df1b1e7596d136d0575308
Koched92 <hichem.koched@gmail.com>
2022-03-26 20:13:34 +0100

    AdminLTE install

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Decision;
-use App\Entity\NatureDossier;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +12,10 @@ class HomeController extends AbstractController
     public function index(ManagerRegistry $doctrine): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         $user = $this->getUser();
+
+        
         
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
